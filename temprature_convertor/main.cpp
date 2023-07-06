b922105cf3502910614b637136a35be3ab035394
M-Farag <mina.farag@icloud.com>
2023-07-06 23:37:07 +0300

    Initial Commit

---
+++ temprature_convertor/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  temprature_convertor
+//
+//  Created by Mina on 06/07/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
