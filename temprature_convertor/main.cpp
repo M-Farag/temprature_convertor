71ec71f429b77fd3f51ab85406730056f546b38e
M-Farag <mina.farag@icloud.com>
2023-07-06 23:45:33 +0300

    ADD Temperature convertors

--- temprature_convertor/main.cpp
+++ temprature_convertor/main.cpp
@@ -1,1 +1,16 @@
+#include "iostream"
 
+double convertToCelsius(double degree)
+{
+    return (degree - 32.0) * 0.5556;
+}
+
+double convertToFahrenheit(double degree)
+{
+    return (degree * 1.8 ) + 32.0;
+}
+
+
+int main() {
+    std::cout << "Hello, Write a degree and 'c' to convert to 'Celsius' & 'f' to convert to 'Fahrenheit' \n";
+}
