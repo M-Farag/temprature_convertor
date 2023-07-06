99652e70c88960f52f06b2da1694d068fb05aff6
M-Farag <mina.farag@icloud.com>
2023-07-06 23:37:39 +0300

    ADD Init

--- temprature_convertor/main.cpp
+++ temprature_convertor/main.cpp
@@ -1,14 +1,1 @@
-//
-//  main.cpp
-//  temprature_convertor
-//
-//  Created by Mina on 06/07/2023.
-//
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
